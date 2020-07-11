b5b8fd39037de63d3d57804999f41cde12f47663
Daniel Silva <danielrodriguesdrs331@gmail.com>
2020-07-11 17:20:13 -0300

    NEW: Create header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import "./App.scss";
-import { Notification, Section } from 'rbx';
+import Header from "./components/header";
 
 const App = () => (
     <div>
-        <Section>
-            <Notification color="success">
-                Notes
-            </Notification>
-        </Section>
+        <Header />
     </div>
 )
 
